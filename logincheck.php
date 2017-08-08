0ea18891aa7a858715c89dc9b4e2ba5aae699c34
doyle95 <doyle230295@gmail.com>
2017-08-08 16:12:50 +1000

    Add files via upload

--- logincheck.php
+++ logincheck.php
@@ -4,7 +4,7 @@
 
 
 
-    try {
+
         // open connection to MongoDB server
         $conn = new MongoClient();
 
@@ -19,28 +19,22 @@
         $userPass = $_POST['password'];
 
 
-        $user = $db->$collection->findOne(array('userName'=> $userName, 'passWord'=> $userPass));
+        $result = $db->$collection->findOne(array('userName'=> $userName, 'passWord'=> $userPass));
 
-        foreach ($user as $obj) {
-            echo 'Username' . $obj['username'];
-            echo 'password: ' . $obj['password'];
-            if($userName == 'user1' && $userPass == 'pass1'){
-                echo 'found';
-            }
-            else{
-                echo 'not found';
-            }
 
+
+        if(!empty($result)){
+            echo "You are successfully loggedIn";
+        }else{
+            echo "Wrong combination of username and password";
         }
+
+
 
         // disconnect from server
         $conn->close();
 
-    } catch (MongoConnectionException $e) {
-        die('Error connecting to MongoDB server');
-    } catch (MongoException $e) {
-        die('Error: ' . $e->getMessage());
-    }
+
 
 
 
